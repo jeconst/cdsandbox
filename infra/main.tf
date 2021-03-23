26e09bc6e2cdea821ef2719cfa31b4620d942033
Justin Constantino <justin.constantino@gmail.com>
2021-03-23 00:29:10 -0500

    WIP: Load balancer with httpd placeholder

--- infra/main.tf
+++ infra/main.tf
@@ -21,6 +21,37 @@ provider "aws" {
 
 data "aws_region" "current" {}
 
+resource "aws_cloudwatch_log_group" "this" {
+  name = "cdsandbox"
+}
+
+resource "aws_lb" "this" {
+  name            = "cdsandbox"
+  subnets         = [for subnet in aws_subnet.public : subnet.id]
+  security_groups = [aws_security_group.load_balancer.id]
+}
+
+resource "aws_lb_target_group" "this" {
+  for_each = toset(["blue", "green"])
+
+  name        = "cdsandbox-${each.value}"
+  vpc_id      = aws_vpc.this.id
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+}
+
+resource "aws_lb_listener" "prod_http" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.this["blue"].arn
+  }
+}
+
 resource "aws_ecs_cluster" "this" {
   name = "cdsandbox"
 }
@@ -30,14 +61,57 @@ resource "aws_ecr_repository" "this" {
   image_tag_mutability = "IMMUTABLE"
 }
 
+resource "aws_ecs_task_definition" "placeholder" {
+  family                   = "cdsandbox_placeholder"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  execution_role_arn       = "arn:aws:iam::397731487442:role/ecsTaskExecutionRole" # FIXME
+
+  cpu    = 256
+  memory = 512
+
+  container_definitions = jsonencode([
+    {
+      name         = "web"
+      image        = "httpd:2.4"
+      portMappings = [{ containerPort = 80 }]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-region        = data.aws_region.current.name
+          awslogs-group         = aws_cloudwatch_log_group.this.name
+          awslogs-stream-prefix = "ecs"
+        }
+      }
+    }
+  ])
+}
+
 resource "aws_ecs_service" "this" {
-  name          = "cdsandbox_web"
-  cluster       = aws_ecs_cluster.this.id
-  desired_count = 1
+  name    = "cdsandbox-web"
+  cluster = aws_ecs_cluster.this.id
+
+  launch_type     = "FARGATE"
+  task_definition = aws_ecs_task_definition.placeholder.arn
+  desired_count   = 1
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.this["blue"].arn
+    container_name   = "web"
+    container_port   = 80
+  }
 
   deployment_controller {
-    type = "EXTERNAL"
+    type = "CODE_DEPLOY"
   }
+
+  network_configuration {
+    security_groups  = [aws_security_group.web_server.id]
+    subnets          = [for subnet in aws_subnet.public : subnet.id]
+    assign_public_ip = true
+  }
+
+  depends_on = [aws_lb.this]
 }
 
 output "deployment_target" {
@@ -47,8 +121,5 @@ output "deployment_target" {
     cluster_name   = aws_ecs_cluster.this.name
     service_name   = aws_ecs_service.this.name
     subnets        = [for subnet in aws_subnet.public : subnet.id]
-
-    # TODO: Add ALB and only allow ALB ingress
-    security_groups = [aws_security_group.web_traffic.id]
   }
 }
--- infra/vpc.tf
+++ infra/vpc.tf
@@ -18,7 +18,7 @@ resource "aws_vpc" "this" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "cdsandbox-vpc"
+    Name = "cdsandbox"
   }
 }
 
@@ -49,24 +49,87 @@ resource "aws_subnet" "public" {
   }
 }
 
-resource "aws_security_group" "web_traffic" {
-  name        = "web_traffic"
-  description = "HTTP and HTTPS traffic"
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "cdsandbox"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  for_each = local.availability_zone_cidr_blocks
+
+  subnet_id      = aws_subnet.public[each.key].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_security_group" "web_server" {
+  name        = "webserver"
+  description = "Web server behind load balancer"
   vpc_id      = aws_vpc.this.id
+}
 
-  ingress {
-    protocol         = "tcp"
-    from_port        = 80
-    to_port          = 80
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
+resource "aws_security_group_rule" "web_server_ingress_from_lb" {
+  security_group_id        = aws_security_group.web_server.id
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = 80
+  to_port                  = 80
+  source_security_group_id = aws_security_group.load_balancer.id
+}
 
-  ingress {
-    protocol         = "tcp"
-    from_port        = 443
-    to_port          = 443
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
+resource "aws_security_group_rule" "web_server_egress_all" {
+  security_group_id = aws_security_group.web_server.id
+  type              = "egress"
+  protocol          = "all"
+  from_port         = 0
+  to_port           = 0
+  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]
+}
+
+
+resource "aws_security_group" "load_balancer" {
+  name        = "loadbalancer"
+  description = "Internet-facing load balancer"
+  vpc_id      = aws_vpc.this.id
+}
+
+resource "aws_security_group_rule" "load_balancer_ingress_internet_http" {
+  security_group_id = aws_security_group.load_balancer.id
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 80
+  to_port           = 80
+  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]
+}
+
+resource "aws_security_group_rule" "load_balancer_ingress_internet_https" {
+  security_group_id = aws_security_group.load_balancer.id
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 443
+  to_port           = 443
+  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]
+}
+
+resource "aws_security_group_rule" "load_balancer_egress_to_web_server" {
+  security_group_id        = aws_security_group.load_balancer.id
+  type                     = "egress"
+  protocol                 = "tcp"
+  from_port                = 80
+  to_port                  = 80
+  source_security_group_id = aws_security_group.web_server.id
 }
